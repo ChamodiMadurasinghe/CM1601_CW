8af63f3f2bed24fd54cdbb0613bff0a77d2477a7
ChamodiMadurasinghe <chamodimadurasinghe12@gmail.com>
2026-06-27 11:30:04 +0530

    Modified Main.java

--- src/main/java/org/example/coursework/Main.java
+++ src/main/java/org/example/coursework/Main.java
@@ -1,10 +1,12 @@
 package org.example.coursework;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main (String[] args) {
         LSM sReader = new LSM("inventoy_legacy.txt");
         sReader.lowStock(5);
 
-    }
 
+        dataCleaner iReader = new dataCleaner("inventoy_legacy.txt");
+        iReader.cleanInventory("inventory_clean.txt");
+    }
 }
