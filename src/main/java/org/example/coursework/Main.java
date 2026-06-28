65afa8d7587f8fe43f4d0993cf067d5318c04b43
ChamodiMadurasinghe <chamodimadurasinghe12@gmail.com>
2026-06-28 18:52:31 +0530

    Modified Main.java

--- src/main/java/org/example/coursework/Main.java
+++ src/main/java/org/example/coursework/Main.java
@@ -5,8 +5,10 @@ public class Main {
         LSM sReader = new LSM("inventoy_legacy.txt");
         sReader.lowStock(5);
 
-
-        dataCleaner iReader = new dataCleaner("inventoy_legacy.txt");
+        inventoryDataCleaner iReader = new inventoryDataCleaner("inventoy_legacy.txt");
         iReader.cleanInventory("inventory_clean.txt");
+
+        dealerDataCleaner dReader = new dealerDataCleaner("dealers_legacy.txt");
+        dReader.cleanLegacy("dealer_clean.txt");
     }
 }
