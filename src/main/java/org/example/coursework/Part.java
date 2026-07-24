40371731fa631b06a5228dbfd269b4e22ee3440a
ChamodiMadurasinghe <chamodimadurasinghe12@gmail.com>
2026-07-24 16:25:31 +0530

    Modified Part class

--- src/main/java/org/example/coursework/Part.java
+++ src/main/java/org/example/coursework/Part.java
@@ -21,12 +21,35 @@ public class Part {
         this.imageFile = imageFile;
     }
 
-    public String getId() {
-        return id;
-    }
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
 
-    public void setId(String id) {
-        this.id = id;
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getBrand() {return brand;}
+    public void setBrand(String brand) {this.brand = brand;}
+
+    public double getPrice() {return price;}
+    public void setPrice(double price) {this.price = price;}
+
+    public int getQuantity() {return quantity;}
+    public void setQuantity(int quantity) {this.quantity = quantity;}
+
+    public String getCategory() {return category;}
+    public void setCategory(String category) {this.category = category;}
+
+    public String getDate() {return date;}
+    public void setDate(String date) {this.date = date;}
+
+    public String getImageFile() {return imageFile;}
+    public void setImageFile(String imageFile) {this.imageFile = imageFile;}
+
+    @Override
+    public String toString(){
+        return id + "," + name + "," + brand + "," + String.format("%.2f", price) + "," + quantity + "," + category +
+                "," + date + "," + imageFile;
+
     }
 }
 
