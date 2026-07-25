71bf3c784ff2b8327d3d2f10290ce36458245fdb
ChamodiMadurasinghe <chamodimadurasinghe12@gmail.com>
2026-07-25 15:04:09 +0530

    Modified LSM class

--- src/main/java/org/example/coursework/LSM.java
+++ src/main/java/org/example/coursework/LSM.java
@@ -3,21 +3,42 @@ package org.example.coursework;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class LSM {
     private String fileName;
+    private  int defaultThreshold;
+    private Map<String, Integer> categoryThresholds;
 
-    public LSM(String fileName){
+    public LSM(String fileName, int defaultThreshold){
         this.fileName = fileName;
+        this.defaultThreshold = defaultThreshold;
+        this.categoryThresholds = new HashMap<>();
     }
 
-    public void lowStock(int threshold){
+    public LSM(String fileName, int defaultThreshold, Map<String,Integer> categoryThresholds) {
+        this.fileName = fileName;
+        this.defaultThreshold = defaultThreshold;
+        this.categoryThresholds = (categoryThresholds != null) ? categoryThresholds : new HashMap<>();
+    }
+
+    public void setThreshold(String category, int threshold) {
+        if (category != null) {
+            categoryThresholds.put(category.trim().toUpperCase(), threshold);
+        }
+    }
+
+    public void lowStock(){
         try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
             String line;
             while ((line = reader.readLine()) != null){
                 String[] fields = line.split("[,|;]");
-                if (fields.length >4) {
+                if (fields.length >5) {
                     String qty = fields[4].trim();
+                    String category = fields[5].trim().toUpperCase();
+
+                    int threshold = categoryThresholds.getOrDefault(category,defaultThreshold);
+
                     try {
                         int qtyI = Integer.parseInt(qty);
                         if (qtyI <= threshold){
