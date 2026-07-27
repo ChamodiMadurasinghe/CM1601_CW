5c17f7512db4d46543807f8d91f07c38d7f3cc3c
ChamodiMadurasinghe <chamodimadurasinghe12@gmail.com>
2026-07-27 08:04:04 +0530

    Modified Main.java

--- src/main/java/org/example/coursework/Main.java
+++ src/main/java/org/example/coursework/Main.java
@@ -1,33 +1,11 @@
 package org.example.coursework;
 
-import java.util.*;
-
 public class Main {
-    public static void main (String[] args) {
-        InventoryDataCleaner iReader = new InventoryDataCleaner("inventoy_legacy.txt");
-        iReader.cleanInventory("inventory_clean.txt");
+    public static void main(String[] args) {
+        CommContext.initialize();
 
-        DealerDataCleaner dReader = new DealerDataCleaner("dealers_legacy.txt");
-        dReader.cleanLegacy("dealer_clean.txt");
-
-        RDS dealerSelector = new RDS("dealer_clean.txt");
-        dealerSelector.displayDealers(4);
-
-        Map<String ,Integer> thresholds = new HashMap<>();
-        thresholds.put("ENGINE" ,5);
-        thresholds.put("ELECTRICAL" ,10);
-        thresholds.put("BODYWORK", 5);
-        thresholds.put("BRAKES", 10);
-
-        LSM sReader = new LSM("inventory_clean.txt",5,thresholds);
-        sReader.lowStock();
-
-        ManageInventory CRUD = new ManageInventory("inventory_clean.txt");
-        CRUD.addPart();
-
-       /* ManageInventory CRUD = new ManageInventory("inventory_clean.txt");
-        CRUD.updatePart();*/
-
+        new LSM().printLSParts(CommContext.manageInventory.getAllPartsSorted());
+        CommContext.dealerSelector.displayDealers(4);
 
     }
 }
